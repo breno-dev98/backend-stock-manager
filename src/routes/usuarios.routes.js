b54c11968857cf137f368c82c58c7480e9837b2d
breno-oliveira98 <breno.oliveira2011@hotmail.com>
2025-04-09 17:09:27 -0300

    protegendo rota de usuarios

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,13 +3,14 @@ import UsuarioController from "../controllers/usuarios.controllers.js";
 import { validate } from "../middlewares/validateRequest.js";
 import { usuarioSchema } from "../validations/usuario.schema.js";
 import { validateUUID } from "../middlewares/validateUUID.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js"
 
 const router = Router()
 
 router.post('/usuarios', validate(usuarioSchema), UsuarioController.create);
 router.get('/usuarios', UsuarioController.getAll);
-router.get('/usuarios/:id', validateUUID, UsuarioController.getById);
-router.put('/usuarios/:id', validateUUID, validate(usuarioSchema), UsuarioController.update);
-router.delete('/usuarios/:id', validateUUID, UsuarioController.delete)
+router.get('/usuarios/:id', authMiddleware, validateUUID, UsuarioController.getById);
+router.put('/usuarios/:id', authMiddleware, validateUUID, validate(usuarioSchema), UsuarioController.update);
+router.delete('/usuarios/:id', authMiddleware, validateUUID, UsuarioController.delete)
 
 export default router;
