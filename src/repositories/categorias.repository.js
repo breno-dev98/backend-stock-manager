8803beeb0e9830614f022c8724aeb91e202e32d2
breno-oliveira98 <breno.oliveira2011@hotmail.com>
2025-04-09 13:15:22 -0300

    criando categorias.repository e seus métodos

---
+++ src/repositories/categorias.repository.js
@@ -0,0 +1,31 @@
+import categorias from "../models/categorias.js";
+
+export default class CategoriaRepository {
+    static async create(data) {
+        return await categorias.create(data);
+    }
+
+    static async findAll() {
+        return await categorias.findAll();
+    }
+
+    static async findById(id) {
+        return await categorias.findByPk(id);
+    }
+
+    static async update(id, data) {
+        const categoria = await categorias.findByPk(id);
+        if (!categoria) return null;
+
+        await categorias.update(data, { where: { id } });
+        return categoria;
+    }
+
+    static async delete(id) {
+        const categoria = await categorias.findByPk(id);
+        if (!categoria) return null;
+
+        await categorias.destroy({ where: { id } });
+        return true;
+    }
+}
